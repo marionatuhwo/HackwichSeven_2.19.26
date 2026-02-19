2f7878b58aaf6820c964d2415691ab6e74ffbb2e
marionatuhwo <ano@hawaii.edu>
2026-02-19 12:21:54 -1000

    Completed Hackwich Seven In Class Portion

--- HackwichSeven_2.19.26/ViewController.swift
+++ HackwichSeven_2.19.26/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    var currentIndex = 0
+    var favoriteFoodsArray: [String] = ["Pizza", "Pasta", "Sushi", "Tacos", "Burger"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        topLabel.text="My Favorite Foods"
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any)
+    {
+    //When the button is pressed, set the “bottomLabel” text to display the first item in the array.
+        bottomLabel.text = favoriteFoodsArray[currentIndex]
+        
+    }
+    
 }
 
